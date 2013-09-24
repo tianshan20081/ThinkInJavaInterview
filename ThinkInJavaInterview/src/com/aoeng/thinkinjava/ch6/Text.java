0dbb2379c40668f8e07711150f374bf08f49f9b0
tianshan20081 <zhangshch2008@gmail.com>
2013-09-24 09:04:45 +0800

    Create ch6

---
+++ ThinkInJavaInterview/src/com/aoeng/thinkinjava/ch6/Text.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.aoeng.thinkinjava.ch6;
+
+/**
+ * @author [ShaoCheng Zhang] Sep 24, 2013:9:04:06 AM
+ * @Email [zhangshch2008@gmail.com]
+ */
+public class Text {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
