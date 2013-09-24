807bb2bd2b83b9b3f3ed6e49270f42a98a4f7a1b
Aoeng Zhang <zhangshch2008@gmail.com>
2013-09-24 09:06:42 +0800

    Create ch5

---
+++ ThinkInJavaInterview/src/com/aoeng/thinkinjava/ch5/Text.java
@@ -0,0 +1,11 @@
+package com.aoeng.thinkinjava.ch5;
+
+public class Text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+	}
+
+}
