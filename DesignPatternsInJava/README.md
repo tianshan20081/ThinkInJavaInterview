1153e33e1a92f5f3aa2cdddde760b570a614a625
tianshan20081 <zhangshch2008@gmail.com>
2013-10-16 14:39:49 +0800

    commit

---
+++ DesignPatternsInJava/README.md
@@ -0,0 +1,9 @@
+Design Patterns In Java (2nd Edition)
+Java 设计模式（第二版）
+1.按照意图可以将设计模式分为五种类型
+	a.接口型模式
+	b.职责型模式
+	c.构造型模式
+	d.操作型模式
+	e.扩展型模式
+	
---
+++ DesignPatternsInJava/src/com/aoeng/dp/cat2/ch5/TestInter.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.aoeng.dp.cat2.ch5;
+
+import com.aoeng.dp.cat1.ch1.inter;
+
+/**
+ * @author [ShaoCheng Zhang] Oct 16, 2013:10:53:24 AM
+ * @Email [zhangshch2008@gmail.com]
+ */
+public class TestInter implements inter {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.aoeng.dp.cat1.ch1.inter#test()
+	 */
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
